035893f161287cae254fffcd7ce017a42da03a62
Matthew Webber <Matthew.Webber@gmail.com>
2016-03-24 15:19:41 -0700

    fix some Milestone bugs

--- js/Milestone.js
+++ js/Milestone.js
@@ -1,38 +1,49 @@
 // class representing a milestone result
 
-function Milestone(start_date, blank_html_element, size, color, animal) {
-  this.start_date = start_date;
-  this.html_element = blank_html_element;
-  this.size = size;
-  this.color = color;
-  this.animal = animal;
+define(function() {
 
-  // TODO
-  this.end_date = this.determine_end_date();
-  this.score = 0; // TODO - for ranking
-  this.is_visible = true;
+	function Milestone(start_date, blank_html_element, size, color, animal) {
+	  this.start_date = start_date;
+	  this.html_element = blank_html_element;
+	  this.size = size;
+	  this.color = color;
+	  this.animal = animal;
 
-  this.fill_in_html_element();
-}
+	  // TODO
+	  this.end_date = this.determine_end_date();
+	  this.score = 0; // TODO - for ranking
+	  this.is_visible = true;
 
-Milestone.prototype.fill_in_html_element = function() {
-	var text = document.createTextNode([this.size, this.color, this.animal].join(' '));
-	res.appendChild(text);
-}
+	  this.fill_in_html_element();
+	}
 
-Milestone.prototype.determine_end_date = function() {
-	// TODO
-}
+  Milestone.prototype = {
+  	constructor: Milestone,
 
-// Enums. Move to a util class?
+		fill_in_html_element: function() {
+			var text = document.createTextNode([this.size, this.color, this.animal].join(' '));
+			this.html_element.appendChild(text);
+		},
 
-Milestone.prototype.TimeUnit = {
-	HOURS : 'hours', 
-	DAYS : 'days',
-}
+		determine_end_date: function() {
+			// TODO
+		},
 
-Milestone.prototype.BaseUnit = {
-	TWO : 'two', 
-	TEN : 'ten',
-}
+		set_visible: function(visible) {
+    	this.html_element.style.display = visible ? "block" : "none";
+		}
+  }
 
+  // TODO: move enums to a util class?
+	Milestone.TimeUnit = {
+		HOURS : 'hours', 
+		DAYS : 'days',
+	}
+
+	Milestone.BaseUnit = {
+		TWO : 'two', 
+		TEN : 'ten',
+	}
+
+	return Milestone;
+});
--- js/loadResults.js
+++ js/loadResults.js
@@ -1,66 +1,69 @@
-// Imports
-var Milestone = require(['js/Milestone.js']).Milestone;
+define(function(require) {
 
-// Set things up
-var startDate = new Date(1983, 4, 5); // TODO - don't hardcode
+  // Imports
+  var Milestone = require('Milestone');
 
-var resultsContainer = document.getElementById("results_container");
-var results = [];
+  // Set things up
+  var startDate = new Date(1983, 4, 5); // TODO - don't hardcode
 
-function updateResults() {
-  console.log("Updating Results");
+  var resultsContainer = document.getElementById("results_container");
+  var results = [];
 
-  var hide_cats = !document.getElementById("check_cats").checked;
-  var hide_dogs = !document.getElementById("check_dogs").checked;
-  var hide_red  = !document.getElementById("check_red").checked;
-  var hide_blue = !document.getElementById("check_blue").checked;
+  function updateResults() {
+    console.log("Updating Results");
 
-  for (result of results) {
-    result.style.display = "block";
+    var hide_cats = !document.getElementById("check_cats").checked;
+    var hide_dogs = !document.getElementById("check_dogs").checked;
+    var hide_red  = !document.getElementById("check_red").checked;
+    var hide_blue = !document.getElementById("check_blue").checked;
 
-    if (hide_cats && result.animal == 'cat') {
-      result.style.display = "none";
-    }
+    for (result of results) {
+      result.set_visible(true);
 
-    if (hide_dogs && result.animal == 'dog') {
-      result.style.display = "none";
-    }
+      if (hide_cats && result.animal === 'cat') {
+        result.set_visible(false);
+      }
 
-    if (hide_red && result.color == 'red') {
-      result.style.display = "none";
-    }
+      if (hide_dogs && result.animal === 'dog') {
+        result.set_visible(false);
+      }
 
-    if (hide_blue && result.color == 'blue') {
-      result.style.display = "none";
-    }
-  }
-}
+      if (hide_red && result.color === 'red') {
+        result.set_visible(false);
+      }
 
-function loadDummyData() {
-  var sizes = ['small', 'big', 'gigantic'];
-  var animals = ['cat', 'dog', 'mouse', 'horse'];
-  var colors = ['red', 'green', 'blue'];
-
-  for (size of sizes) {
-    for (animal of animals) {
-      for (color of colors) {
-        var res = document.createElement("p");
-        var stone = new Milestone(startDate, res, size, color, animal);
-        stone.fill_in_html_element();
-        resultsContainer.appendChild(res);        
-        results.push(stone);
+      if (hide_blue && result.color === 'blue') {
+        result.set_visible(false);
       }
     }
   }
-}
 
-function wireUpInputs() {
-  document.getElementById("check_cats").onchange = updateResults;
-  document.getElementById("check_dogs").onchange = updateResults;
-  document.getElementById("check_red").onchange = updateResults;
-  document.getElementById("check_blue").onchange = updateResults;
-}
+  function loadDummyData() {
+    var sizes = ['small', 'big', 'gigantic'];
+    var animals = ['cat', 'dog', 'mouse', 'horse'];
+    var colors = ['red', 'green', 'blue'];
 
-wireUpInputs();
-loadDummyData();
-updateResults();
+    for (size of sizes) {
+      for (animal of animals) {
+        for (color of colors) {
+          var res = document.createElement("p");
+          var stone = new Milestone(startDate, res, size, color, animal);
+          resultsContainer.appendChild(res);        
+          results.push(stone);
+        }
+      }
+    }
+  }
+
+  function wireUpInputs() {
+    document.getElementById("check_cats").onchange = updateResults;
+    document.getElementById("check_dogs").onchange = updateResults;
+    document.getElementById("check_red").onchange = updateResults;
+    document.getElementById("check_blue").onchange = updateResults;
+  }
+
+  wireUpInputs();
+  loadDummyData();
+  updateResults();  
+});
+
