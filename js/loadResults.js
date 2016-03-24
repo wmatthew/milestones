f0268a38bc030db2396738856b63b54a664cbe0f
Matthew Webber <Matthew.Webber@gmail.com>
2016-03-23 17:17:09 -0700

    rearrange js

--- js/loadResults.js
+++ js/loadResults.js
@@ -1,34 +1,15 @@
-// Load some dummy results.
+// Set things up
 var resultsContainer = document.getElementById("results_container");
 var results = [];
 
-for (var i=1; i<=10; i++) {
-	var res = document.createElement("p");
-	var text = document.createTextNode("Result #" + i);
-	res.appendChild(text);
-	res.value = i;
-
-	results.push(res);
-
-	resultsContainer.appendChild(res);
-}
-
-// Wire up the inputs
-document.getElementById("check_odds").onchange = updateResults;
-document.getElementById("check_evens").onchange = updateResults;
-document.getElementById("check_small").onchange = updateResults;
-
-// Initial Update
-updateResults();
-
 function updateResults() {
-	console.log("Updating Results");
+  console.log("Updating Results");
 
   var hide_odds = !document.getElementById("check_odds").checked;
   var hide_evens = !document.getElementById("check_evens").checked;
   var hide_small = !document.getElementById("check_small").checked;
 
-	for (result of results) {
+  for (result of results) {
     result.style.display = "block";
 
     var current_is_odd = result.value % 2 == 1;
@@ -45,6 +26,26 @@ function updateResults() {
     if (hide_small && current_is_small) {
       result.style.display = "none";
     }
-	}
-  
+  }
 }
+
+function loadDummyData() {
+  for (var i=1; i<=10; i++) {
+  	var res = document.createElement("p");
+  	var text = document.createTextNode("Result #" + i);
+  	res.appendChild(text);
+  	res.value = i;
+  	results.push(res);
+  	resultsContainer.appendChild(res);
+  }
+}
+
+function wireUpInputs() {
+  document.getElementById("check_odds").onchange = updateResults;
+  document.getElementById("check_evens").onchange = updateResults;
+  document.getElementById("check_small").onchange = updateResults;
+}
+
+wireUpInputs();
+loadDummyData();
+updateResults();
